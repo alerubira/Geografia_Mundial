ca62a8cad855688f0fb78acff70e64bc1d7bd0da
alerubira <100098255+alerubira@users.noreply.github.com>
2023-08-22 12:46:17 -0300

    Update GeografiaMu.js

--- GeografiaMu.js
+++ GeografiaMu.js
@@ -14,3 +14,29 @@ fetch("https://restcountries.com/v3.1/all")
 //nombrePais.textContent=(paises[0].altSpellings[3]);
 //console.log(paises[0].altSpellings[3]);
 div1.textContent=(paises[0].flags[0]);
+const userId = 1;
+const updatedUserData = {
+  name: 'Nuevo Nombre',
+  email: 'nuevo@email.com'
+};
+
+fetch(`https://api.example.com/users/${userId}`, {
+  method: 'PUT',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(updatedUserData)
+})
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Error al actualizar');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log('Actualización exitosa:', data);
+  })
+  .catch(error => {
+    console.error('Error al actualizar:', error);
+  });
+
